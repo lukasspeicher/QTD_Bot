f69ef3496f51fc419843c06b87e07b366b7443f5
lukas99 <lukass070@googlemail.com>
2021-05-14 22:57:58 +0200

    First version

---
+++ src/main/java/com/qtd/bot/QTDBot.java
@@ -0,0 +1,37 @@
+package com.qtd.bot;
+
+import discord4j.core.DiscordClientBuilder;
+import discord4j.core.GatewayDiscordClient;
+import discord4j.core.event.domain.lifecycle.ReadyEvent;
+import discord4j.core.event.domain.message.MessageCreateEvent;
+import discord4j.core.object.entity.Message;
+import discord4j.core.object.entity.User;
+
+public class QTDBot {
+
+    public static void main(String[] args) {
+        GatewayDiscordClient client = DiscordClientBuilder.create(args[0])
+                .build()
+                .login()
+                .block();
+
+        client.getEventDispatcher().on(ReadyEvent.class)
+                .subscribe(event -> {
+                    final User self = event.getSelf();
+                    System.out.println(String.format(
+                            "Logged in as %s#%s", self.getUsername(), self.getDiscriminator()
+                    ));
+                });
+
+        client.getEventDispatcher().on(MessageCreateEvent.class)
+                .map(MessageCreateEvent::getMessage)
+                .filter(message -> message.getAuthor().map(user -> !user.isBot()).orElse(false))
+                .filter(message -> message.getContent().equalsIgnoreCase("!ping"))
+                .flatMap(Message::getChannel)
+                .flatMap(channel -> channel.createMessage("Pong!"))
+                .subscribe();
+
+        client.onDisconnect().block();
+    }
+
+}
